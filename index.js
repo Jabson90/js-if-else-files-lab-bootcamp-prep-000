860803b172f98ee07130b453a0167c3717398e8e
Jacob stærk frederiksen <jacob_st@hotmail.com>
2018-05-12 11:00:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 var name = "Joe"
 var height = "74"
-
+if (name === "susan") {
+  console.log ("the name is correct")
+} else {
+  console.log ("expected the name to be susan")
+}
 
 // Don't worry about this
-module.exports
+module.exports = { name, height
+}
 
